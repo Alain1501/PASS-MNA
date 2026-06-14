c4a64076d4a3f6beaf36af62fdf1ca80478d3233
EMV-USP77-HPV <dr.ali.alkhafagi@gmail.com>
2026-06-14 21:44:17 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -24,10 +24,13 @@ def create_app(config_class=Config):
     from app.routes import main_bp
     from app.api import api_bp
     from app.admin import admin_bp
+    from app.setup import setup_bp
 
     app.register_blueprint(auth_bp)
     app.register_blueprint(main_bp)
     app.register_blueprint(api_bp, url_prefix="/api")
     app.register_blueprint(admin_bp)
+    app.register_blueprint(setup_bp)
+
 
     return app
